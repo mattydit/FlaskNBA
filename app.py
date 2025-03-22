84b3d20b2716043a464b8284d6dd6fcc5d7b8170
mattydit <mateusz.harcej27@gmail.com>
2025-03-22 19:46:22 +0000

    updating url for current season

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import urllib.request, json, datetime
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+nbaURL = "https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2024/league/00_full_schedule.json"
 
 class Schedule:
 
@@ -25,7 +26,7 @@ class Schedule:
 
 @app.route('/')
 def get_games():
-    with urllib.request.urlopen("https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2022/league/00_full_schedule.json") as url:
+    with urllib.request.urlopen(nbaURL) as url:
         data = json.load(url)
 
         datenow = str(datetime.date.today())
